c67cc924c065a893aae8b241c0df2d19e7c2971a
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-09 16:20:02 -0300

    TDD iteration

--- Tests/colaSalida.test.js
+++ Tests/colaSalida.test.js
@@ -4,3 +4,11 @@ test("Crear cola salida", () =>{
     var salida= new colaSalida();
     expect(salida.paquetes).toBe(0);
 })
+
+
+
+test("Entra un paquete a la cola de salida", () =>{
+    var salida= new colaSalida();
+    salida.procesarPaquete();
+    expect(salida.paquetes).toBe(1);
+})
