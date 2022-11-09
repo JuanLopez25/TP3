6267d76bac091678c67b2d935d42f02d9f7c3d97
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-09 17:25:57 -0300

    TDD iteration

--- Tests/centroDistribucion.test.js
+++ Tests/centroDistribucion.test.js
@@ -1,7 +1,14 @@
 const CentroDistribucion=require('../src/centroDistribucion')
-
+const Paquete=require('../src/paquete')
 
 test("Crear centro de distribucion", () =>{
     var distribucion = new CentroDistribucion();
     expect(distribucion.paquetes.length).toBe(0);
+})
+
+test("Procesar paquete centro de distribucion", () =>{
+    var distribucion= new CentroDistribucion();
+    var paquete= new Paquete("1")
+    distribucion.procesarPaquete(paquete);
+    expect(distribucion.paquetes.length).toBe(1);
 })
