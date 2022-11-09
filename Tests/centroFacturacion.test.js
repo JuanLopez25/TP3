23dfe400b2171b8619d0b735be4efc80c6cebfb6
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-09 16:47:24 -0300

    TDD iteration

--- Tests/centroFacturacion.test.js
+++ Tests/centroFacturacion.test.js
@@ -1,6 +1,6 @@
-
+const CentroFacturacion=require('../src/centroFacturacion')
 
 test("Crear centro de facturacion", () =>{
-    var facturacion= new centroFacturacion();
+    var facturacion= new CentroFacturacion();
     expect(facturacion.paquetes.length).toBe(0);
 })
---
+++ src/centroFacturacion.js
@@ -0,0 +1,6 @@
+
+function CentroFacturacion() {
+
+}
+
+module.exports=CentroFacturacion;
