58e53a08c793af37aab827af8ab444997a395834
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-10 20:20:52 -0300

    TDD iteration

--- Tests/local.test.js
+++ Tests/local.test.js
@@ -1,7 +1,14 @@
 const Local= require('../src/local')
-
+const Paquete= require('../src/paquete')
 
 test("Verificar cantidad de paquetes en la cola del salida del local", () =>{
     var local= new Local();
     expect(local.colaDeSalida()).toBe(0);
+})
+
+test("Verificar cantidad de paquetes en la cola del salida del local agregando paquetes", () =>{
+    var local= new Local();
+    var paquete= new Paquete();
+    local.agregarPaquete(paquete);
+    expect(local.colaDeSalida()).toBe(1);
 })
