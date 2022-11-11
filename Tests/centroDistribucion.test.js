02311b6450d6b883302aeb2381e8d0b28aedbecf
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 18:36:17 -0300

    TDD iteration

--- Tests/centroDistribucion.test.js
+++ Tests/centroDistribucion.test.js
@@ -1,23 +1,29 @@
 const CentroDistribucion=require('../src/centroDistribucion')
 const Paquete=require('../src/paquete')
 
+var paquete;
+var lista;
+var distribucion;
+
+beforeEach(()=> {
+    distribucion= new CentroDistribucion();
+    paquete = new Paquete("1");
+    lista=[];
+    lista.push(paquete)
+});
+
 test("Crear centro de distribucion", () =>{
-    var distribucion = new CentroDistribucion();
     expect(distribucion.paquetes.length).toBe(0);
 })
 
 test("Procesar paquete centro de distribucion", () =>{
-    var distribucion= new CentroDistribucion();
-    var paquete= new Paquete("1")
-    distribucion.procesarPaquete(paquete);
+    distribucion.procesarPaquete(lista);
     expect(distribucion.paquetes.length).toBe(1);
 })
 
 
 test("Procesar paquete centro de facturacion", () =>{
-    var distribucion= new CentroDistribucion();
-    var paquete= new Paquete("1");
-    distribucion.procesarPaquete(paquete);
-    distribucion.terminarProceso(paquete);
+    distribucion.procesarPaquete(lista);
+    distribucion.terminarProceso(lista);
     expect(distribucion.paquetes.length).toBe(0);
 })
