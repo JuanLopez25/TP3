e91349e63ff5e9c8fa1f18c92b17414a52c5e14a
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 19:40:48 -0300

    TDD iteration

--- Tests/destino.test.js
+++ Tests/destino.test.js
@@ -1,6 +1,22 @@
 const Destino=require('../src/destino');
+const Paquete= require('../src/paquete')
+
+var paquete;
+var lista;
+var destino;
+
+beforeEach(()=> {
+    destino= new Destino();
+    paquete = new Paquete("1");
+    lista=[];
+    lista.push(paquete)
+});
 
 test("Verificar cantidad de paquetes en el destino", () =>{
-    var destino= new Destino();
     expect(destino.paquetes.length).toBe(0);
+})
+
+test("Recibir paquete destino", () =>{
+    calidad.procesarPaquete(lista);
+    expect(calidad.paquetes.length).toBe(1);
 })
