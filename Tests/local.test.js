524183d47d10ff20f420a589fbd1d68cd04856b9
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 18:30:23 -0300

    TDD iteration

--- Tests/local.test.js
+++ Tests/local.test.js
@@ -92,3 +92,13 @@ test("Verificar cantidad de paquetes en centro de distribucion", () =>{
     var local= new Local();
     expect(local.paquetesCentroDeDistribucion()).toBe(0);
 })
+
+test("Verificar cantidad de paquetes en centro de distribucion agregando 1 paquete y procesandolo para que llegue al centro de distribucion", () =>{
+    var local= new Local();
+    var paquete= new Paquete();
+    local.agregarPaquete(paquete);
+    local.avanzarTiempo();
+    local.avanzarTiempo();
+    local.avanzarTiempo();
+    expect(local.paquetesCentroDeDistribucion()).toBe(1);
+})
