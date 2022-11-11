f16f3b81a9ba4541c18c3403a8c05e449d3267f0
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 18:53:14 -0300

    TDD iteration

--- Tests/local.test.js
+++ Tests/local.test.js
@@ -1,24 +1,31 @@
 const Local= require('../src/local')
 const Paquete= require('../src/paquete')
 
+var paquete1;
+var paquete2;
+var lista;
+var local;
+
+beforeEach(()=> {
+    local= new Local();
+    paquete1 = new Paquete("1");
+    paquete2= new Paquete("2");
+    lista=[];
+    lista.push(paquete)
+});
+
 test("Verificar cantidad de paquetes en la cola del salida del local", () =>{
-    var local= new Local();
     expect(local.paquetesColaDeSalida()).toBe(0);
 })
 
 test("Verificar cantidad de paquetes en la cola del salida del local agregando paquetes", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
+    local.agregarPaquete(lista);
     expect(local.paquetesColaDeSalida()).toBe(1);
 })
 
-
 test("Verificar cantidad de paquetes en la cola del salida del local agregando 2 paquetes", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
-    local.agregarPaquete(paquete);
+    lista.push(paquete2);
+    local.agregarPaquete(lista);
     expect(local.paquetesColaDeSalida()).toBe(2);
 })
 
@@ -27,27 +34,20 @@ test("Verificar cantidad de paquetes en la cola del salida del local agregando 2
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
 
-
 test("Verificar cantidad de paquetes en centro de facturacion", () =>{
-    var local= new Local();
     expect(local.paquetesCentroDeFacturacion()).toBe(0);
 })
 
-
 test("Verificar cantidad de paquetes en centro de facturacion agregando 1 paquete y procesandolo para que llegue al centro de facturacion", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
+    local.agregarPaquete(lista);
     local.avanzarTiempo();
     expect(local.paquetesCentroDeFacturacion()).toBe(1);
 })
 
 
 test("Verificar cantidad de paquetes en centro de facturacion agregando 2 paquetes y procesandolos para que lleguen al centro de facturacion", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
-    local.agregarPaquete(paquete);
+    lista.push(paquete2);
+    local.agregarPaquete(lista);
     local.avanzarTiempo();
     expect(local.paquetesCentroDeFacturacion()).toBe(2);
 })
@@ -59,25 +59,20 @@ test("Verificar cantidad de paquetes en centro de facturacion agregando 2 paquet
 
 
 test("Verificar cantidad de paquetes en centro de calidad", () =>{
-    var local= new Local();
     expect(local.paquetesCentroDeCalidad()).toBe(0);
 })
 
 
 test("Verificar cantidad de paquetes en centro de calidad agregando 1 paquete y procesandolo para que llegue al centro de calidad", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
+    local.agregarPaquete(lista);
     local.avanzarTiempo();
     local.avanzarTiempo();
     expect(local.paquetesCentroDeCalidad()).toBe(1);
 })
 
 test("Verificar cantidad de paquetes en centro de calidad agregando 2 paquetes y procesandolos para que lleguen al centro de calidad", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
-    local.agregarPaquete(paquete);
+    lista.push(paquete2);
+    local.agregarPaquete(lista);
     local.avanzarTiempo();
     local.avanzarTiempo();
     expect(local.paquetesCentroDeCalidad()).toBe(2);
@@ -89,14 +84,11 @@ test("Verificar cantidad de paquetes en centro de calidad agregando 2 paquetes y
 //-----------------------------------------------------------------------------------------------------------------
 
 test("Verificar cantidad de paquetes en centro de distribucion", () =>{
-    var local= new Local();
     expect(local.paquetesCentroDeDistribucion()).toBe(0);
 })
 
 test("Verificar cantidad de paquetes en centro de distribucion agregando 1 paquete y procesandolo para que llegue al centro de distribucion", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
+    local.agregarPaquete(lista);
     local.avanzarTiempo();
     local.avanzarTiempo();
     local.avanzarTiempo();
@@ -104,10 +96,8 @@ test("Verificar cantidad de paquetes en centro de distribucion agregando 1 paque
 })
 
 test("Verificar cantidad de paquetes en centro de distribucion agregando 1 paquete y procesandolo para que llegue al centro de distribucion", () =>{
-    var local= new Local();
-    var paquete= new Paquete();
-    local.agregarPaquete(paquete);
-    local.agregarPaquete(paquete);
+    lista.push(paquete2);
+    local.agregarPaquete(lista);
     local.avanzarTiempo();
     local.avanzarTiempo();
     local.avanzarTiempo();
