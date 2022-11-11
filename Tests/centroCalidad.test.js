502093f76cec3b16cefa024362362ce1c86e2f8b
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 18:22:58 -0300

    TDD iteration

--- Tests/centroCalidad.test.js
+++ Tests/centroCalidad.test.js
@@ -1,22 +1,29 @@
 const CentroCalidad= require('../src/centroCalidad')
 const Paquete = require('../src/paquete')
+
+var paquete;
+var lista;
+var calidad;
+
+beforeEach(()=> {
+    calidad= new CentroCalidad();
+    paquete = new Paquete("1");
+    lista=[];
+    lista.push(paquete)
+});
+
 test("Crear centro de Calidad", () =>{
-    var calidad= new CentroCalidad();
     expect(calidad.paquetes.length).toBe(0);
 })
 
 
 test("Procesar paquete centro de calidad", () =>{
-    var calidad= new CentroCalidad();
-    var paquete= new Paquete("1")
     calidad.procesarPaquete(paquete);
     expect(calidad.paquetes.length).toBe(1);
 })
 
 
 test("Procesar paquete centro de calidad", () =>{
-    var calidad = new CentroCalidad();
-    var paquete = new Paquete("1");
     calidad.procesarPaquete(paquete);
     calidad.terminarProceso(paquete);
     expect(calidad.paquetes.length).toBe(0);
