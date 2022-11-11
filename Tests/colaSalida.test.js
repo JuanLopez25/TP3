a904d865fbe59b158331c887533c59f95c408aba
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 18:57:02 -0300

    TDD iteration

--- Tests/colaSalida.test.js
+++ Tests/colaSalida.test.js
@@ -1,25 +1,31 @@
 const ColaSalida= require('../src/colaSalida');
 const Paquete = require('../src/paquete');
 
+var paquete;
+var lista;
+var salida;
+
+beforeEach(()=> {
+    salida= new ColaSalida();
+    paquete = new Paquete("1");
+    lista=[];
+    lista.push(paquete)
+});
+
 test("Crear cola salida", () =>{
-    var salida= new ColaSalida();
     expect(salida.paquetes.length).toBe(0);
 })
 
 
 
 test("Entra un paquete a la cola de salida", () =>{
-    var salida= new ColaSalida();
-    var paquete= new Paquete("1")
-    salida.procesarPaquete(paquete);
+    salida.procesarPaquete(lista);
     expect(salida.paquetes.length).toBe(1);
 })
 
 
 test("Termino de procesar paquetes", () =>{
-    var salida= new ColaSalida();
-    var paquete= new Paquete("1");
-    salida.procesarPaquete(paquete);
+    salida.procesarPaquete(lista);
     salida.terminarProceso();
     expect(salida.paquetes.length).toBe(0);
 })
