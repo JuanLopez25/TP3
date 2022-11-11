428b06fbd1ab1bb1d15d5b58123eeae12e387d45
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-11 19:24:20 -0300

    TDD iteration

--- Tests/destino.test.js
+++ Tests/destino.test.js
@@ -1,4 +1,4 @@
-const Destino=require('./destino');
+const Destino=require('../src/destino');
 
 test("Verificar cantidad de paquetes en el destino", () =>{
     expect(Destino.paquetes.length).toBe(0);
