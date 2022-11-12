f7fe14c8d26ab5a4ddfcbbf9677adef5642daac6
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-12 09:25:27 -0300

    TDD iteration

--- src/centroCalidad.js
+++ src/centroCalidad.js
@@ -6,8 +6,10 @@ function CentroCalidad() {
     this.cola= [];
     this.procesarPaquete = function(paquetesAgregar) {
         var i=0;
-        this.cola= this.cola.concat(paquetesAgregar)
         var paqueteTemporal;
+        
+        this.agregarACola(paquetesAgregar);
+
         while (i<((this.cola).length)) {
             if (this.paquetes.length<1) {
                 paqueteTemporal= this.cola[i];
@@ -23,6 +25,14 @@ function CentroCalidad() {
         this.paquetes = [];
         return entrega;
     }
+
+    this.agregarACola = function (paquetesAgregar) {
+        var i=0;
+        while ((this.cola.length)<6  &&  i<paquetesAgregar.length) {  //es menor que 6 por que 1 puede ser procesado, y 5 a la cola de espera
+            this.cola.push(paquetesAgregar[i]);
+            i++;
+        }
+    } 
 }
 
 module.exports=CentroCalidad;
