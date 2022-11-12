2ebd4b3b84d812316b92dc177f47191245c670c5
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 10:14:40 -0300

    TDD iteration

--- Tests/paquete.test.js
+++ Tests/paquete.test.js
@@ -1,6 +1,11 @@
 const Paquete=require('../src/paquete')
 
-test("Crear Paquete con urgencia", () =>{
+test("Crear Paquete con urgencia muy rapido", () =>{
     var paquete= new Paquete("1",[],"muy rapido");
     expect(paquete.urgencia).toBe(4);
+})
+
+test("Crear Paquete con urgencia rapido", () =>{
+    var paquete= new Paquete("1",[],"rapido");
+    expect(paquete.urgencia).toBe(6);
 })
