69775d06111da260c23fa597b71f25af5a94c59d
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 11:49:48 -0300

    TDD iteration

--- Tests/localConUrgencia.test.js
+++ Tests/localConUrgencia.test.js
@@ -23,24 +23,20 @@ test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
 })
 
 test("5 paquetes a centro de Facturacion y procesa los 3 con mayor urgencia", () =>{
-    local.avanzarTiempo(1);   //facturacion procesando 3 y 2 en cola
+    local.avanzarTiempo(1);   
     var lista2= [paqueteMuyRapido,paqueteMuyRapido, paqueteMuyRapido, paqueteMuyRapido, paqueteMuyRapido, paqueteMuyRapido];
-    local.agregarPaquetes(lista2);  //Cola de salida agrendando 5
-    local.avanzarTiempo(1); //facturacion procesando 3 y cola de 4 
+    local.agregarPaquetes(lista2); 
+    local.avanzarTiempo(1); 
     var urgenciaTotal=0;
     local.centroDeFacturacion.paquetes.forEach(elemento => urgenciaTotal+=elemento.urgencia);
     expect(urgenciaTotal).toBe(12);
 })
 
 test("3 paquetes a centro de calidad y procesa el de mayor urgencia", () =>{
-    local.avanzarTiempo(2); ///centro de calidad procesa el primer muy rapidos y en la cola de espera el primero es muy rapido y el segundo es rapido
-    // el centro de facturacion esta procesando el rapido y el normal
+    local.avanzarTiempo(2); 
     var lista2= [paqueteMuyRapido,paqueteMuyRapido, paqueteMuyRapido, paqueteMuyRapido, paqueteMuyRapido, paqueteMuyRapido];
-    local.agregarPaquetes(lista2);  //Cola de salida agrendando 5 muy rapidos
-    local.avanzarTiempo(1); //facturacion esta procesando 3 muy rapidos
-    //centro de calidad esta procesando el muy rapido y en cola tiene [rapido,rapido,normal]
-    local.avanzarTiempo(1); //facturacion procesando los 2 muy rapidos
-    //centro de calidad deberia procesar un muy rapido pero en cola el orden es [rapido,rapido,normal,muy rapido, muy rapido, muy rapido]
+    local.agregarPaquetes(lista2);
+    local.avanzarTiempo(2);  
     var urgenciaTotal=0;
     local.centroDeCalidad.paquetes.forEach(elemento => urgenciaTotal+=elemento.urgencia);
     expect(urgenciaTotal).toBe(4);
