e6653b9fc8c002ba50ecee32beab8704247161f4
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 10:18:25 -0300

    TDD iteration

--- src/paquete.js
+++ src/paquete.js
@@ -2,7 +2,12 @@
 function Paquete(destinoPropuesto,productos,urgencia) {
        this.destino=destinoPropuesto;
        this.productos=productos;
-       this.urgencia=4;
+       switch(urgencia){
+            case "muy rapido":
+                this.urgencia=4;
+            case "rapido":
+                this.urgencia=6;
+       }
 }
 
 module.exports= Paquete;
