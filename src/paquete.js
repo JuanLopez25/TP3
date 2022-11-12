6ba8baef5590200dbe949e2e218122c4510c783b
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 12:24:10 -0300

    TDD iteration

--- src/paquete.js
+++ src/paquete.js
@@ -1,18 +1,26 @@
 
-function Paquete(destinoPropuesto,productos,urgencia) {
-       this.destino=destinoPropuesto;
-       this.productos=productos;
-       switch(urgencia){
-            case "muy rapido":
-                this.urgencia=4;
-                break;
-            case "rapido":
-                this.urgencia=6;
-                break;
-            case "normal":
-                this.urgencia=8;
-                break;
-       }
+function Paquete(id,destinoPropuesto,productos,urgencia) {
+    this.destino=destinoPropuesto;
+    this.productos=productos;
+    this.tiempo=0;
+    switch(urgencia){
+        case "muy rapido":
+            this.urgencia=4;
+            break;
+        case "rapido":
+            this.urgencia=6;
+            break;
+        case "normal":
+            this.urgencia=8;
+            break;
+    }
+    this.informe = function() {
+        if(this.tiempo<=this.urgencia) {
+            "P"+this.id+"Destino "+this.destino+", Urgencia "+this.urgencia+", llego a tiempo\n"
+        } else {
+            "P"+this.id+"Destino "+this.destino+", Urgencia "+this.urgencia+", llego a tiempo\n"
+        }
+    }
 }
 
 module.exports= Paquete;
