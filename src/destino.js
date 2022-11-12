68713f9293290111df8c542bd461dcc61728ec86
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 12:52:33 -0300

    TDD iteration

--- src/destino.js
+++ src/destino.js
@@ -14,6 +14,7 @@ function Destino(numero){
     this.informarLlegadas= function(){
         var respuesta="";
         this.paquetes.forEach(elemento => respuesta+=elemento.informe());
+        this.paquetes=[];
         return respuesta;
     }
     
