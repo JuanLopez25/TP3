308300aafd910fed2bcce47979ebc99def5e1d3a
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 09:58:17 -0300

    TDD iteration

--- src/paquete.js
+++ src/paquete.js
@@ -1,24 +1,6 @@
 
-
 function paquete(destinoPropuesto) {
-    this.destino=destinoPropuesto;
-    this.posicionEstado = 0;
-    this.estados = ["Local","Centro de facturacion", "Centro de calidad", "Centro de distribucion",destinoPropuesto];
-
-
-    this.tiempo=0;
-    
-    this.estadoPaquete = function () {
-        return this.estados[this.posicionEstado];
-    }
-
-
-    this.siguienteEtapa = function () {
-        this.tiempo+=1;
-        if ((this.posicionEstado+1)<(this.estados.length)){
-            this.posicionEstado+=1;
-        }
-    }    
+       
 }
 
 module.exports= paquete;
