0a8fb14a6ad3d8291a8a417b2ce2183174a23c71
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 10:51:43 -0300

    TDD iteration

--- Tests/localConUrgencia.test.js
+++ Tests/localConUrgencia.test.js
@@ -1,14 +1,29 @@
 const Local= require('../src/local')
 const Paquete= require('../src/paquete')
 
-test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
-    var paqueteMuyRapido=new Paquete("1",[],"muy rapido");
-    var paqueteRapido=new Paquete("1",[],"rapido");
-    var paqueteNormal=new Paquete("1",[],"normal");
-    var local= new Local("A");
-    var lista=[paqueteMuyRapido,paqueteNormal,paqueteNormal,paqueteRapido,paqueteRapido,paqueteMuyRapido];
+var paqueteMuyRapido
+var paqueteRapido
+var paqueteNormal
+var local
+var lista
+
+beforeEach(()=> {
+    paqueteMuyRapido=new Paquete("1",[],"muy rapido");
+    paqueteRapido=new Paquete("1",[],"rapido");
+    paqueteNormal=new Paquete("1",[],"normal");
+    local= new Local("A");
+    lista=[paqueteMuyRapido,paqueteNormal,paqueteNormal,paqueteRapido,paqueteRapido,paqueteMuyRapido];
     local.agregarPaquete(lista);
+});
+
+test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
     var urgenciaTotal=0;
     local.colaDeSalida.paquetes.forEach(elemento => urgenciaTotal+=elemento.urgencia);
     expect(urgenciaTotal).toBe(28);
+})
+
+test("5 paquetes a centro de Facturacion y procesa los 3 con mayor urgencia", () =>{
+    var urgenciaTotal=0;
+    local.centroDeFacturacion.paquetes.forEach(elemento => urgenciaTotal+=elemento.urgencia);
+    expect(urgenciaTotal).toBe(14);
 })
