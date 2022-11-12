9bfdcaad3ac2509e3bbf3491fcbd33a54a62b21a
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-12 10:41:22 -0300

    TDD iteration

---
+++ Tests/localConUrgencia.test.js
@@ -0,0 +1,14 @@
+const Local= require('../src/local')
+const Paquete= require('../src/paquete')
+
+test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
+    var paqueteMuyRapido=new Paquete("1",[],"muy rapido");
+    var paqueteRapido=new Paquete("1",[],"rapido");
+    var paqueteNormal=new Paquete("1",[],"normal");
+    var local= new Local("A");
+    var lista=[paqueteMuyRapido,paqueteNormal,paqueteNormal,paqueteRapido,paqueteRapido,paqueteMuyRapido];
+    local.agregarPaquete(lista);
+    var urgenciaTotal=0;
+    local.colaDeSalida.paquetes.forEach(elemto => urgenciaTotal+=elemento.urgencia);
+    expect(urgenciaTotal).toBe(28);
+})
