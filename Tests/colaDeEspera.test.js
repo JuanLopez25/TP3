a2bf7490801707e4e0a3767d5dfd1f6769a41b4d
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-13 14:50:32 -0300

    TDD iteration

--- Tests/colaDeEspera.test.js
+++ Tests/colaDeEspera.test.js
@@ -10,7 +10,7 @@ beforeEach(()=> {
     lista=[];
     var i=1;
     while (i<=50) {
-        var paquete= new Paquete(i,[],"normal",4);
+        paquete= new Paquete(i,[],"normal",4);
         lista.push(paquete) 
         i++;
     }
