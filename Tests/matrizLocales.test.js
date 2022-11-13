60918ab8b15b5765ae52fa51247faa1bdd350b6b
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-13 17:18:44 -0300

    TDD iteration

--- Tests/matrizLocales.test.js
+++ Tests/matrizLocales.test.js
@@ -2,5 +2,7 @@ const MatrizLocales=require('../src/matrizLocales');
 
 test("Crear matriz de locales", () =>{
     var matriz= new MatrizLocales(4,["CF","CC","CD"]);
+    var strNombres=matriz.locales[0].nombre+matriz.locales[1].nombre+matriz.locales[2].nombre+matriz.locales[3].nombre;
     expect(matriz.locales.length).toBe(4);
+    expect(strNombres).toBe("ABCD");
 })
