09d899123209e285813c1b677d83e097269ae110
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-11-13 14:42:51 -0300

    TDD iteration

--- Tests/centroDistribucion.test.js
+++ Tests/centroDistribucion.test.js
@@ -7,9 +7,19 @@ var distribucion;
 
 beforeEach(()=> {
     distribucion= new CentroDistribucion();
-    paquete = new Paquete("1");
+    paquete1 = new Paquete(1,[],"muy rapido",4);
+    paquete2 = new Paquete(2,[],"normal",4);
+    paquete3 = new Paquete(3,[],"muy rapido",4);
+    paquete4 = new Paquete(4,[],"rapido",4);
+    paquete5 = new Paquete(5,[],"normal",4);
+    paquete6 = new Paquete(6,[],"normal",4);
+    paquete7 = new Paquete(7,[],"normal",4);
+    paquete8 = new Paquete(8,[],"normal",4);
+    paquete9 = new Paquete(9,[],"normal",4);
+    paquete10 = new Paquete(10,[],"normal",4);
+    paquete11 = new Paquete(11,[],"normal",4);
     lista=[];
-    lista.push(paquete)
+    lista.push(paquete1)
 });
 
 test("Crear centro de distribucion", () =>{
@@ -29,39 +39,38 @@ test("Procesar paquete centro de facturacion", () =>{
 })
 
 test("Entran 10 paquetes al centro de distribucion", () =>{
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
+    lista.push(paquete2);
+    lista.push(paquete3);
+    lista.push(paquete4);
+    lista.push(paquete5);
+    lista.push(paquete6);
+    lista.push(paquete7);
+    lista.push(paquete8);
+    lista.push(paquete9);
+    lista.push(paquete10);
     distribucion.procesarPaquetes(lista);
     expect(distribucion.paquetes.length).toBe(10);
 })
 
 
 test("Entran 11 paquetes al centro de distribucion y deberian entrar nada mas que 10 ya que este es el limite", () =>{
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
-    lista.push(paquete);
+    lista.push(paquete2);
+    lista.push(paquete3);
+    lista.push(paquete4);
+    lista.push(paquete5);
+    lista.push(paquete6);
+    lista.push(paquete7);
+    lista.push(paquete8);
+    lista.push(paquete9);
+    lista.push(paquete10);
+    lista.push(paquete11);
     distribucion.procesarPaquetes(lista);
     expect(distribucion.paquetes.length).toBe(10);
 })
 
 test("Unir paquetes con mismo destino", () =>{
-    var paquete1=new Paquete(1,[],"muy rapido",4);
-    var paquete2=new Paquete(1,[],"normal",4);
-    var lista=[paquete1,paquete2];
+    var paqueteAux=new Paquete(1,[],"normal",4);
+    lista.push(paqueteAux);
     distribucion.procesarPaquetes(lista);
     expect(distribucion.paquetes.length).toBe(1);
 })
