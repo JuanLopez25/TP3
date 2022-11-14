0675451820ae4716b05537d2bc3fdf3694f11bea
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-14 00:10:32 -0300

    TDD iteration

--- src/matrizLocales.js
+++ src/matrizLocales.js
@@ -52,7 +52,7 @@ function MatrizLocales(localesAgregar,centros,limitesColasDeEspera){
             columna++;
         }
 
-        console.log(this.locales.informarPaquetesEnDestino());
+        console.log("informa: "+this.locales.informarPaquetesEnDestino());
         // this.locales.forEach(local => {
         //     console.log("El local "+local.nombre);
         //     local.centrosCreados.forEach(elemento =>{
