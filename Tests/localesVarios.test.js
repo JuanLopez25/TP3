42b267a267ef03815785cb0905ef069d0e12104c
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-13 23:54:19 -0300

    TDD iteration

--- Tests/localesVarios.test.js
+++ Tests/localesVarios.test.js
@@ -26,50 +26,50 @@ beforeEach(()=> {
     lista=[paqueteMuyRapido,paqueteNormal,paqueteNormal2,paqueteRapido,paqueteRapido2,paqueteMuyRapido2];
 });
 
-test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
-    matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[[3,2,5,24,6]]);
-    matriz.agregarPaquetes(lista,"A");
-    var urgenciaTotal=0;
-    matriz.locales[0].centrosCreados[0].paquetes.forEach(elemento => urgenciaTotal+=elemento.urgencia);
-    expect(urgenciaTotal).toBe(28);
-})
+// test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
+//     matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[[3,2,5,24,6]]);
+//     matriz.agregarPaquetes(lista,"A");
+//     var urgenciaTotal=0;
+//     matriz.locales[0].centrosCreados[0].paquetes.forEach(elemento => urgenciaTotal+=elemento.urgencia);
+//     expect(urgenciaTotal).toBe(28);
+// })
 
-test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro de facturacion estar procesando 3 paquetes", () =>{
-    matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[[4,3,4,23,6]]);
-    matriz.agregarPaquetes(lista,"A");
-    matriz.avanzarTiempo();  
-    expect(matriz.locales[0].centrosCreados[1].paquetes.length).toBe(3);
-})
+// test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro de facturacion estar procesando 3 paquetes", () =>{
+//     matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[[4,3,4,23,6]]);
+//     matriz.agregarPaquetes(lista,"A");
+//     matriz.avanzarTiempo();  
+//     expect(matriz.locales[0].centrosCreados[1].paquetes.length).toBe(3);
+// })
 
 // test("6 paquetes a cola de salida y proceso dos veces por lo que deberia el centro de calidad estar procesando 1 paquete", () =>{
-//     matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[4,2,3,25,6]);
+//     matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[[4,2,3,25,6]]);
 //     matriz.agregarPaquetes(lista,"A");
 //     matriz.avanzarTiempo();
 //     matriz.avanzarTiempo();    
 //     expect(matriz.locales[0].centrosCreados[2].paquetes.length).toBe(1);
 // })
 
-// test("6 paquetes a cola de salida y proceso tres veces por lo que deberia el centro de calidad estar procesando 1 paquete", () =>{
-//     matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[3,2,4,17,5]);
-//     matriz.agregarPaquetes(lista,"A");
-//     matriz.avanzarTiempo();  
-//     matriz.avanzarTiempo();  
-//     matriz.avanzarTiempo();  
-//     expect(matriz.locales[0].centrosCreados[3].paquetes.length).toBe(1);
-// })
-// ///-----------------------
-// ///-----------------------
+test("6 paquetes a cola de salida y proceso tres veces por lo que deberia el centro de calidad estar procesando 1 paquete", () =>{
+    matriz= new MatrizLocales(1,["CF","CC","CC","CD","CF"],[[3,2,4,17,5]]);
+    matriz.agregarPaquetes(lista,"A");
+    matriz.avanzarTiempo();  
+    matriz.avanzarTiempo();  
+    matriz.avanzarTiempo();  
+    expect(matriz.locales[0].centrosCreados[3].paquetes.length).toBe(1);
+})
+///-----------------------
+///-----------------------
 
 
 // test("6 paquetes a cola de salida y toma los 5 de mayor urgencia", () =>{
-//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[30,22,4,3,15]);
+//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[[30,22,4,3,15]]);
 //     matriz.agregarPaquetes(lista,"A");
 //     expect(matriz.locales[0].centrosCreados[0].paquetes.length).toBe(5);
 // })
 
 
 // test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro de distribucion estar procesando 5 paquetes", () =>{
-//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[13,15,3,2,28]);
+//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[[13,15,3,2,28]]);
 //     matriz.agregarPaquetes(lista,"A");
 //     matriz.avanzarTiempo();  
 //     expect(matriz.locales[0].centrosCreados[1].paquetes.length).toBe(5);
@@ -77,7 +77,7 @@ test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro
 
 
 // test("6 paquetes a cola de salida y proceso dos veces por lo que deberia el centro de distribucion estar procesando 5 paquetes", () =>{
-//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[24,30,3,2,25]);
+//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[[24,30,3,2,25]]);
 //     matriz.agregarPaquetes(lista,"A");
 //     matriz.avanzarTiempo();  
 //     matriz.avanzarTiempo();  
@@ -86,7 +86,7 @@ test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro
 
 
 // test("6 paquetes a cola de salida y proceso tres veces por lo que deberia el centro de facturacion estar procesando 3 paquetes", () =>{
-//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[14,13,4,2,29]);
+//     matriz= new MatrizLocales(1,["CD","CD","CF","CC","CD"],[[14,13,4,2,29]]);
 //     matriz.agregarPaquetes(lista,"A");
 //     matriz.avanzarTiempo();  
 //     matriz.avanzarTiempo();  
@@ -97,27 +97,27 @@ test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro
 // //chequear que se deban tener uno de cada uno
 
 // test("Creo un local que no cumpla los requisitos y debe agregarse el CF y el CC", () =>{
-//     matriz= new MatrizLocales(1,["CD"],[14]);
+//     matriz= new MatrizLocales(1,["CD"],[[14]]);
 //     expect(matriz.locales[0].centrosCreados[2].nombre).toBe("CF");
 // })
 
 
 // test("Creo un local que no cumpla los requisitos y debe agregarse el CF y el CC", () =>{
-//     matriz= new MatrizLocales(1,["CD"],[14]);
+//     matriz= new MatrizLocales(1,["CD"],[[14]]);
 //     expect(matriz.locales[0].centrosCreados[3].nombre).toBe("CC");
 // })
 
 // test("Creo un local que no cumpla los requisitos y debe agregarse el CF y el CC", () =>{
-//     matriz= new MatrizLocales(1,["CD"],[14]);
+//     matriz= new MatrizLocales(1,["CD"],[[14]]);
 //     expect(matriz.locales[0].centrosCreados[4].nombre).toBe("D");
 // })
 
 // test("Creo un local que no cumpla los requisitos y agrego CC", () =>{
-//     matriz= new MatrizLocales(1,["CD","CF","CD","CD","CD","CD"],[15,3,15,16,14,13]);
+//     matriz= new MatrizLocales(1,["CD","CF","CD","CD","CD","CD"],[[15,3,15,16,14,13]]);
 //     expect(matriz.locales[0].centrosCreados[7].nombre).toBe("CC");
 // })
 // test("Creo un local que no cumpla losrequisitos y agrego CC", () =>{
-//     matriz= new MatrizLocales(1,["CD","CF","CD","CD","CD","CD"],[15,4,30,25,24,20]);
+//     matriz= new MatrizLocales(1,["CD","CF","CD","CD","CD","CD"],[[15,4,30,25,24,20]]);
 //     expect(matriz.locales[0].centrosCreados[8].nombre).toBe("D");
 // })
 
@@ -125,7 +125,7 @@ test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro
 //     var reseter= new Paquete("1",[],"muy rapido");
 //     reseter.resetearID();
 //     var centrosAgregar= ["CF","CD","CF","CC"];
-//     matriz= new MatrizLocales(1,centrosAgregar,[3,25,4,3]);
+//     matriz= new MatrizLocales(1,centrosAgregar,[[3,25,4,3]]);
 //     matriz.agregarPaquetes([new Paquete("1",[],"muy rapido",centrosAgregar.length+1)],"A");
 //     matriz.avanzarTiempo();  
 //     matriz.avanzarTiempo();  
@@ -139,7 +139,7 @@ test("6 paquetes a cola de salida y proceso una vez por lo que deberia el centro
 //     var reseter= new Paquete("1",[],"muy rapido");
 //     reseter.resetearID();
 //     var centrosAgregar= ["CF","CD","CF","CF","CC"];
-//     matriz= new MatrizLocales(1,centrosAgregar,[4,25,3,4,2]);
+//     matriz= new MatrizLocales(1,centrosAgregar,[[4,25,3,4,2]]);
 //     matriz.agregarPaquetes([new Paquete("1",[],"muy rapido",centrosAgregar.length+1)],"A");
 //     matriz.avanzarTiempo();  
 //     matriz.avanzarTiempo();  
