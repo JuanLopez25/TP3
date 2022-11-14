a77b55c8b839d47dac299f75ed2fe76099d039bd
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-14 16:38:25 -0300

    TDD iteration

--- Tests/localConUrgencia.test.js
+++ Tests/localConUrgencia.test.js
@@ -76,6 +76,8 @@ test("Comparar urgencia con tiempo de llegada del segundo paquete muy rapido, el
     matriz.avanzarTiempo();
     console.log(matriz.locales[0].centrosCreados[4].paquetes);
     console.log(matriz.locales[0].informarPaquetesEnDestino()+"hola");
+    
     matriz.avanzarTiempo();   //Llega el primer paquete, el "muy rapido"
+    console.log(matriz.locales[0].centrosCreados[4].paquetes);
     expect(matriz.locales[0].informarPaquetesEnDestino()).toBe("P2: Destino 1, Urgencia 4, no llego a tiempo\n");
 })
