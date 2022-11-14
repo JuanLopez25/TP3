36c6be7080dc19c55520d111463c21b6e31b78d9
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-14 14:30:18 -0300

    TDD iteration

--- Tests/matrizPaquetesEnDiagonal.test.js
+++ Tests/matrizPaquetesEnDiagonal.test.js
@@ -100,6 +100,8 @@ test("Procesamos y verificamos donde estan los paquetes con destino 3", () =>{
 
 
 test("Procesamos y verificamos donde estan los paquetes con destino 3", () =>{
+    reseter2= new Local(["CF","CC","CD"],[6,2,14]);
+    reseter2.resetearID();
     var matriz2= new MatrizLocales(4,["CF","CC","CD"],[[6,3,23],[5,4,10],[6,2,14],[6,3,20]]);
     lista.push(new Paquete(4,[],"rapido",4));
     matriz2.agregarPaquetes(lista,"A");
