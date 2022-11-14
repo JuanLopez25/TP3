c70f1fd7e8c393ab1a6b463dff3f54df00bd9435
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-14 20:37:24 -0300

    TDD iteration

--- src/colaSalida.js
+++ src/colaSalida.js
@@ -1,29 +1,26 @@
-
 const FuncionesCentros= require('./moduloFuncionesCentros');
 
+
 function ColaSalida(paquete) {
-    this.paquetes=[];
-    this.cola=[];
-    this.nombre="CS";
-    this.colaSalida=[];
-    this.funcionesCentros=FuncionesCentros;  
-    this.procesarPaquetes = function() {
-      this.funcionesCentros.procesarPaquetes(this.cola,this.paquetes,5);
-    }
-    
-    
-    this.terminarProceso = function() {
-      this.paquetes.forEach(paquete=>{
-          this.colaSalida.push(paquete);
-      });
-      this.paquetes = [];
-      var entrega=[]
-      this.colaSalida.forEach(paquete=>{
-          entrega.push(paquete);
-      });
+  this.paquetes=[];
+  this.cola=[];
+  this.nombre="CS";
+  this.colaSalida=[];
+  this.funcionesCentros=FuncionesCentros;
+
+  this.procesarPaquetes = function(paquetesAProcesar) {
+    this.agregarACola(paquetesAProcesar);
+    this.funcionesCentros.procesarPaquetes(this.cola,this.paquetes,5);
+  }
+  this.terminarProceso = function() {
+      var entrega=this.funcionesCentros.terminarProceso(this.paquetes,this.colaSalida);
+      this.paquetes=[];
       this.colaSalida=[];
       return entrega;
-    }
+  }
+  this.agregarACola = function (paquetesAgregar) {
+      this.funcionesCentros.agregarACola(this.cola,this.limiteCola,paquetesAgregar);
+  }
 }
 
 module.exports= ColaSalida;
