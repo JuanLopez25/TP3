a1a14ba0d8e82d7519e3c815046237db162aa608
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-13 23:35:25 -0300

    TDD iteration

--- src/local.js
+++ src/local.js
@@ -55,7 +55,6 @@ var Local = (function(){
         //---------------------------------------------------------------------
         
         this.agregarPaquetes= function(paquetes) {
-            console.log("llegue")
             this.centrosCreados[0].procesarPaquetes(paquetes);
         }
 
