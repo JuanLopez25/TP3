66cc4a578a55c296ab6287b6609e295aec0906eb
JuanLopez25 <j.lopez.s.252001@gmail.com>
2022-11-14 14:36:25 -0300

    TDD iteration

--- Tests/matrizPaquetesEnDiagonal.test.js
+++ Tests/matrizPaquetesEnDiagonal.test.js
@@ -126,5 +126,5 @@ test("Procesamos y verificamos donde estan los paquetes con destino 3", () =>{
     matriz2.avanzarTiempo(1);
     matriz2.avanzarTiempo(1);
     matriz2.avanzarTiempo(1);
-    expect(matriz2.locales[2].informarPaquetesEnDestino()).toBe("P1: Destino 1, Urgencia 4, llego a tiempo\nP4: Destino 3, Urgencia 6, llego a tiempo\n");
+    expect(matriz2.locales[0].informarPaquetesEnDestino()).toBe("P1: Destino 1, Urgencia 4, llego a tiempo\nP4: Destino 3, Urgencia 6, llego a tiempo\n");
 })
